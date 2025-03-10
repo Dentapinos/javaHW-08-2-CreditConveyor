9d624ea66e8d6aa3d12bd1f1d6b2e25970e106f0
DenisLadchenkov <119222041+Dentapinos@users.noreply.github.com>
2025-03-10 13:24:53 +0300

    refactor lombok

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class Order {
+    @Setter(AccessLevel.NONE)
     private int creditRatingAmendment = 0;
 
     String firstName;
@@ -17,113 +26,7 @@ public class Order {
     int LoanTermInMonths;
     int salary;
 
-    public Order(String firstName, String secondName, String surname, int age, Country nationality, Bank bindingSalaryToTheBank, int workExperienceInLastPlace, int totalWorkExperience, double desiredLoanAmount, int loanTermInMonths, int salary) {
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.surname = surname;
-        this.age = age;
-        this.nationality = nationality;
-        this.bindingSalaryToTheBank = bindingSalaryToTheBank;
-        this.workExperienceInLastPlace = workExperienceInLastPlace;
-        this.totalWorkExperience = totalWorkExperience;
-        this.desiredLoanAmount = desiredLoanAmount;
-        LoanTermInMonths = loanTermInMonths;
-        this.salary = salary;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public int getLoanTermInMonths() {
-        return LoanTermInMonths;
-    }
-
-    public void setLoanTermInMonths(int loanTermInMonths) {
-        LoanTermInMonths = loanTermInMonths;
-    }
-
-    public double getDesiredLoanAmount() {
-        return desiredLoanAmount;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setNationality(Country nationality) {
-        this.nationality = nationality;
-    }
-
-    public Bank getBindingSalaryToTheBank() {
-        return bindingSalaryToTheBank;
-    }
-
-    public void setBindingSalaryToTheBank(Bank bindingSalaryToTheBank) {
-        this.bindingSalaryToTheBank = bindingSalaryToTheBank;
-    }
-
-    public int getWorkExperienceInLastPlace() {
-        return workExperienceInLastPlace;
-    }
-
-    public void setWorkExperienceInLastPlace(int workExperienceInLastPlace) {
-        this.workExperienceInLastPlace = workExperienceInLastPlace;
-    }
-
-    public int getTotalWorkExperience() {
-        return totalWorkExperience;
-    }
-
-    public void setTotalWorkExperience(int totalWorkExperience) {
-        this.totalWorkExperience = totalWorkExperience;
-    }
-
-    public void setDesiredLoanAmount(double desiredLoanAmount) {
-        this.desiredLoanAmount = desiredLoanAmount;
-    }
-
-    public int getCreditRatingAmendment() {
-        return creditRatingAmendment;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public void setCreditRatingAmendment() {
         this.creditRatingAmendment += 1;
-    }
-
-    public Country getNationality() {
-        return nationality;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public String getSurname() {
-        return surname;
     }
 }
